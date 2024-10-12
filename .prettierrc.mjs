afaaaca6758badfb2da661b15e2dc31953e4a9c1
it1shka <2tishbel@gmail.com>
2024-10-12 14:09:03 +0200

    feat(pages): added Main page

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+const prettierConfig = Object.freeze({
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  printWidth: 80,
+})
+
+export default prettierConfig
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { memo } from "react"
-
-const App = () => {
-  return <div>Privet!</div>
-}
-
-export default memo(App)
